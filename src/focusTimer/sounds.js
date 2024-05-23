730fc6c567f75ae908483ed1aab8879a0a7e5c80
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-05-23 10:37:15 -0300

    Correção de bugs

--- src/focusTimer/sounds.js
+++ src/focusTimer/sounds.js
@@ -3,4 +3,7 @@ export const rain = new Audio("./assets/Chuva.wav");
 export const storefront = new Audio("./assets/Cafeteria.wav");
 export const fire = new Audio("./assets/Lareira.wav");
 
-
+tree.loop = true
+rain.loop = true
+storefront.loop = true
+fire.loop = true
